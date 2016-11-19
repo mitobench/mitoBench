4a74453adcb459d42c0bb2ffaae1cdc8a88e045d
peltzer <alexander.peltzer@uni-tuebingen.de>
2016-11-19 20:19:28 +0100

    Started working on the testing parts....

---
+++ src/test/java/test/IOTests.java
@@ -0,0 +1,43 @@
+package test;
+
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by peltzer on 18/11/2016.
+ */
+public class IOTests {
+    private InputStream is;
+    private InputStreamReader isr;
+    private BufferedReader bfr;
+
+
+    private void setUp(String path){
+
+    }
+
+
+    @Test
+    public void io_test_fasta(){
+
+    }
+
+    @Test
+    public void io_test_genericInput(){
+
+    }
+
+    @Test
+    public void io_test_hsd(){
+
+    }
+
+
+
+
+
+}
