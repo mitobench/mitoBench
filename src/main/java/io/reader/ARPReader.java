6ff48363e2b4cb4af85967086f2786fe3bcd1809
peltzer <alexander.peltzer@uni-tuebingen.de>
2016-11-25 18:07:02 +0100

    Started working on ARP reader with Groupings in the table.

---
+++ src/main/java/io/reader/ARPReader.java
@@ -0,0 +1,20 @@
+package io.reader;
+
+import io.IInputData;
+import io.datastructure.Entry;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by peltzer on 24/11/2016.
+ */
+public class ARPReader implements IInputData {
+
+
+
+    @Override
+    public HashMap<String, List<Entry>> getCorrespondingData() {
+        return null;
+    }
+}
