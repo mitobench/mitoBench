a49d65a32a18e5f078aedfd8e823433a05ca69fe
peltzer <alexander.peltzer@uni-tuebingen.de>
2016-11-28 16:06:04 +0100

    ARP is back!

--- src/main/java/view/table/ImportDialogue.java
+++ src/main/java/view/table/ImportDialogue.java
@@ -40,6 +40,7 @@ public class ImportDialogue extends Application {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Multi-FastA Input (*.fa, *.fasta, *.fas)", "*.fasta", "*.fa", "*.fas"),
                 new FileChooser.ExtensionFilter("Haplogrep 2 HSD Format (*.hsd)", "*.hsd"),
+                new FileChooser.ExtensionFilter("ARP Arlequin Input Format (*.arp)", "*.arp"),
                 new FileChooser.ExtensionFilter("Generic Input Format (*.tsv)", "*.tsv")
         );
     }
