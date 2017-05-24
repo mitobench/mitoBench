4865ede95b07883796d448eaedd70f4b34cfc325
JudithNeulamm <judith.neukamm@gmx.net>
2017-05-24 15:27:20 +0200

    update test

--- src/test/java/gui/GUITests.java
+++ src/test/java/gui/GUITests.java
@@ -90,7 +90,7 @@ public class GUITests extends FxRobot implements GUITestValidator {
     public void testWalkThrough() throws Exception {
         GUITestSteps steps = new GUITestSteps(this);
 
-        steps.part4BasicStuff();
+        //steps.part4BasicStuff();
         //steps.part3AboutDialogueTests();
         //steps.part2FillTable(getResource(testFiles.getProject_file()).toString());
         //steps.part5DBTest();
