fd70ccab8e8dcf17eec7d65479466e2724415587
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2017-08-14 11:45:48 +0200

    Authors ++

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'mitoBench'
-copyright = '2017, Judith Neukamm'
-author = 'Judith Neukamm'
+copyright = '2017, Judith Neukamm and Alexander Peltzer'
+author = 'Judith Neukamm, Alexander Peltzer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
