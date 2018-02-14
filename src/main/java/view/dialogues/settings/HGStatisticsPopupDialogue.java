75f78ccb783b138b6e07ffe9e34e31b79a105160
JudithNeukamm <afW91L:C>
2018-02-14 16:15:54 +0100

    set 'default hg list' to 'example hg list'

--- src/main/java/view/dialogues/settings/HGStatisticsPopupDialogue.java
+++ src/main/java/view/dialogues/settings/HGStatisticsPopupDialogue.java
@@ -16,9 +16,8 @@ public class HGStatisticsPopupDialogue extends ATabpaneDialogue {
 
     private TextField textField_hglist;
     private Button okBtn;
-    private CheckBox default_list_checkbox;
+    private CheckBox example_hg_list_checkbox;
     private HaploStatistics haploStatistics;
-    private Scene scene;
     private TabPane statsTabPane;
 
     public HGStatisticsPopupDialogue(String title, LogClass LOGClass){
@@ -42,7 +41,7 @@ public class HGStatisticsPopupDialogue extends ATabpaneDialogue {
         this.haploStatistics = haploStatistics;
         Label label = new Label("Please enter comma separated list of haplogroups " +
                 "\naccording to which the haplogroups should be grouped:");
-        Label default_list = new Label("or use the default list:");
+        Label example_hg_list = new Label("or use example list:");
 
         textField_hglist = new TextField();
         if(haploStatistics.getChartController().getCustomHGList()!=null) {
@@ -56,26 +55,27 @@ public class HGStatisticsPopupDialogue extends ATabpaneDialogue {
         okBtn = new Button("OK");
         okBtn.setId("button_ok_statistics");
 
-        default_list_checkbox = new CheckBox("Use default list");
-        default_list_checkbox.setId("checkbox_hg_default_selection");
-        default_list_checkbox.setSelected(false);
+        example_hg_list_checkbox = new CheckBox("Use example list");
+        example_hg_list_checkbox.setId("checkbox_hg_example_selection");
+        example_hg_list_checkbox.setSelected(false);
 
         dialogGrid.add(label, 0,0,3,1);
         dialogGrid.add(textField_hglist, 0,1,3,1);
-        dialogGrid.add(default_list,0,2,1,1);
-        dialogGrid.add(default_list_checkbox,1,2,1,1);
+        dialogGrid.add(example_hg_list,0,2,1,1);
+        dialogGrid.add(example_hg_list_checkbox,1,2,1,1);
         dialogGrid.add(okBtn,2,3,1,1);
     }
 
     public void addListener(){
         okBtn.setOnAction(e -> {
-            if((textField_hglist.getText().equals("") || textField_hglist.getText().startsWith("Please")) &&  !default_list_checkbox.isSelected()){
+            if((textField_hglist.getText().equals("") || textField_hglist.getText().startsWith("Please")) &&  !example_hg_list_checkbox.isSelected()){
                 textField_hglist.setText("Please enter list here.");
 
             } else {
                 String[] hg_list;
-                if(default_list_checkbox.isSelected()){
+                if(example_hg_list_checkbox.isSelected()){
                     hg_list = haploStatistics.getChartController().getCoreHGs();
+                    textField_hglist.setText(Arrays.toString(haploStatistics.getChartController().getCoreHGs()));
                 } else {
                     hg_list = textField_hglist.getText().split(",");
                 }
@@ -102,12 +102,12 @@ public class HGStatisticsPopupDialogue extends ATabpaneDialogue {
 
         });
 
-        Tooltip tp = new Tooltip("Default list : H,HV,I,J,K,L0,L1,L2,L3,L4,M1,N,N1a,N1b,R,R0,T,T1,T2,U,W,X");
-        default_list_checkbox.setOnMouseEntered(event -> {
-            Point2D p = default_list_checkbox.localToScreen(default_list_checkbox.getLayoutBounds().getMaxX(), default_list_checkbox.getLayoutBounds().getMaxY()); //I position the tooltip at bottom right of the node (see below for explanation)
-            tp.show(default_list_checkbox, p.getX(), p.getY());
+        Tooltip tp = new Tooltip("Example list : H,HV,I,J,K,L0,L1,L2,L3,L4,M1,N,N1a,N1b,R,R0,T,T1,T2,U,W,X");
+        example_hg_list_checkbox.setOnMouseEntered(event -> {
+            Point2D p = example_hg_list_checkbox.localToScreen(example_hg_list_checkbox.getLayoutBounds().getMaxX(), example_hg_list_checkbox.getLayoutBounds().getMaxY()); //I position the tooltip at bottom right of the node (see below for explanation)
+            tp.show(example_hg_list_checkbox, p.getX(), p.getY());
         });
-        default_list_checkbox.setOnMouseExited(event -> tp.hide());
+        example_hg_list_checkbox.setOnMouseExited(event -> tp.hide());
     }
 
 
